49c6c16c88b8add0e3408dfba5def81b0d8332fe
Sam Welek <s.welek@outlook.com>
2015-11-28 22:09:13 +0000

    Much improved menu system for the console side UI, This one allows only one key press (without having to press the enter/return key) and is much more easier to extend.

--- WikipediaAPIUtilities/WikipediaAPIUtilities/Program.cs
+++ WikipediaAPIUtilities/WikipediaAPIUtilities/Program.cs
@@ -10,16 +10,32 @@ namespace WikipediaAPIUtilities
     {
         static void Main(string[] args)
         {
+            showOptions();
+        }
+
+        private static void showOptions()
+        {
             Console.WriteLine("Welcome to the Wikipedia API Utilities Console Application");
-            Console.WriteLine("Please select the option below");
-            Console.WriteLine("1) WikiWars");
-            var result = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("Please select an option below");
+            Console.WriteLine("1 - WikiWars");
+            ConsoleKeyInfo _choice = Console.ReadKey();
+            string _strChoice = _choice.KeyChar.ToString();
+            startProgram(_strChoice);
+        }
 
-            //Convert.ToInt32(result);
-
-            if (result == "1")
+        private static void startProgram(string _strChoice)
+        {
+            Console.Clear();
+            if (_strChoice == "1")
             {
                 Wikiwars w = new Wikiwars();
+            }
+            else
+            {
+                Console.WriteLine("'" + _strChoice + "' is not a valid input, please try again.");
+                Console.WriteLine();
+                showOptions();
             }
         }
     }
